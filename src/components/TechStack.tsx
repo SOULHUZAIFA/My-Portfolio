a26e695dca0e440ab07e2d0c248749045399c083
Huzaifa Ambakhutwala <huzaifamehdi53@gmail.com>
2025-05-21 13:18:47 -0500

    added new techstacks

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -21,6 +21,9 @@ const imageUrls = [
   "/images/mysql.webp",
   "/images/typescript.webp",
   "/images/javascript.webp",
+  "/images/metasploit.webp",
+  "/images/git.webp",
+  "/images/netcore.webp"
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
