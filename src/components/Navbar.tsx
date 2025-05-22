91fcab57c0af6264ed9b19b75738eaf34c838f2d
Huzaifa Ambakhutwala <huzaifamehdi53@gmail.com>
2025-05-21 23:14:11 -0500

    minor changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          HuzaifaAmbakhutwala
+          Huzaifa
         </a>
         <a
           href="mailto:huzaifa.ambakhutwala@gmail.com"
